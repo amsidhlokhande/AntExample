4d0163c4e94eebe8c99e1605a16ed5deab8c6d79
amsidhlokhande <amsidhlokhande@gmail.com>
2020-04-19 22:01:27 +0530

    Initial Commit

---
+++ SimpleWebApplicationUsingAnt/src/com/vaannila/service/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.vaannila.service;
+
+import com.vaannila.domain.User;
+
+public class UserServiceImpl implements UserService {
+
+	public void add(User user) {
+		System.out.println("User added successfully");
+
+	}
+
+}
